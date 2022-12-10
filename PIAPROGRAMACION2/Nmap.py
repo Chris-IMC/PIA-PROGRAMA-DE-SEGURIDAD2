db3cd981791a5cd2808c4a214d763322e53a1a88
Chris-IMC <101905696+Chris-IMC@users.noreply.github.com>
2022-12-09 23:28:50 -0600

    Add files via upload

---
+++ PIAPROGRAMACION2/Nmap.py
@@ -0,0 +1,44 @@
+import nmap
+import csv
+import os
+import logging
+import PIAPROGRAMACION2Prueba as PIA
+#148.234.5.206
+logging.basicConfig(level=logging.INFO, filemode="Tareas_de_ciberseguridad.log", format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+
+class Nmap:
+    def DireccecionIp(self):
+        try:
+            while True:
+                print("introduzca la direccion ip que desea escanear: ")
+                direccion_ip = int("IP:")
+                if not direccion_ip:
+                    print("Vueva a intentar introducir una direccion IP")
+                else:
+                    break
+
+        except Exception as error:
+            logging.error(error, exc_info=True)
+            print(" Hay un problema con la direccion")
+
+        return direccion_ip
+
+    def Nmap(self):
+        try:
+            scanner_nmap = ScannerNmap()
+            IP = scanner_nmap.DireccionIP()
+            scanner = nmap.PortScanner()
+            sc = scanner.scan(hosts=IP, arguments='--open')
+            hosts_list = [(x, sc['scan'][x]['hostnames'][0]['name'], list(sc['scan'][x]['tcp'].keys())) for x in
+                      scanner.all_hosts()]
+            for item in hosts_list:
+                with open('scanHosts1.csv', 'a', newline='') as csvf:
+                    writer = csv.writer(csvf)
+                    writer.writerow(item)
+
+        except Exception as error:
+            logging.error(error, exc_info=True)
+            print("Hay un problema")
+
